68ec55aadb9154a29d8a916b6092cefde28b442f
ravikanth <ravikanthreddy89@gmail.com>
2014-02-11 19:31:04 -0500

    Sender event and receive evnet are now anonymous threads not functions

--- nt2.py
+++ nt2.py
@@ -2,7 +2,9 @@ from socket import AF_INET, SOCK_DGRAM
 import sys
 import socket
 import select
-import struct, time, thread;
+import thread;
+import struct, time, threading;
+
 
 class AgTime:
 	lClock=0;
@@ -33,71 +35,83 @@ class Ntp:
 		t -= self.TIME1970
 		return t;
 
+class server(threading.Thread):
+	def __init__(self,portNo):
+		threading.Thread.__init__(self);
+		self.PORTNO=portNo;
+		self.server_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+		self.server_socket.bind(("",self.PORTNO));
+		self.CONNECTION_LIST=[];
+		self.RECV_BUFFER=4096;
 
-def server(portno):
-	# List to keep track of socket descriptors
-    CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
-    PORT = portno;
-     
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # this has no effect, why ?
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind(("", PORT))
-    server_socket.listen(10)
- 
-    # Add server socket to the list of readable connections
-    CONNECTION_LIST.append(server_socket)
- 
-    print "Server started on port " + str(PORT)
- 
-    while 1:
-        # Get the list sockets which are ready to be read through select
-        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
- 
-        for sock in read_sockets:
-            #New connection
-            if sock == server_socket:
-                # Handle the case in which there is a new connection recieved through server_socket
-                sockfd, addr = server_socket.accept()
-                CONNECTION_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
-                 
-             
-            #Some incoming message from a client
-            else:
-                # Data recieved from client, process it
-                try:
-                    data = sock.recv(RECV_BUFFER)
-                    if data:
-                	print data; 
-                except:
-                    print "Client (%s, %s) is offline" % addr
-                    sock.close()
-                    CONNECTION_LIST.remove(sock)
-                    continue
-     
-    server_socket.close()
-	
+	def run(self):
+		self.server_socket.listen(10);
+		self.CONNECTION_LIST.append(self.server_socket);
+		print "Server started on port :"+str(self.PORTNO);
+		
+		while(1):
+			read_sockets,write_sockets,error_sockets=select.select(self.CONNECTION_LIST,[],[]);
+			for sock in read_sockets:
+				if sock==self.server_socket:
+					sockfd, addr= self.server_socket.accept();
+					self.CONNECTION_LIST.append(sockfd);
+					print "Message rxd from (%s %s)",addr
+				else:
+					try:
+						data=sock.recv(self.RECV_BUFFER);
+						if data:
+							thread.start_new_thread(recv,(data,));
+					except:
+						sock.close();
+						self.CONNECTION_LIST.remove(sock);
+						continue;
+		self.server_socket.close();
+
+def recv(data):
+	lock.acquire();
+	print data;
+	lock.release();
+
 
 def send(data,portno):
+	lock.acquire();
+	t_stamp=AgTime();
+	t_stamp.lClock=max(time_stamp.lClock,time_stamp.pClock);
+	if(t_stamp.lClock==time_stamp.lClock):
+		time_stamp.count=time_stamp.count+1;
+	else:
+		time_stamp.count=0;
+	t_stamp.pClock=time_stamp.pClock;
+	t_stamp.count=time_stamp.count;
+
+	#update the time_stamp object to hold the event timestamp
+	time_stamp.lClock=t_stamp.lClock;
+	time_stamp.pClock=t_stamp.pClock;
+	time_stamp.count=t_stamp.count;
 	client_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 	client_socket.connect(("localhost",portno));
 	client_socket.send(data);
 	client_socket.close();
-
+	lock.release();
 
 
 if __name__ == "__main__":
-	#create an ntp object
+	#create a global lock for mutual exclusion
+	global lock;
+	lock=threading.RLock();
+        #create an ntp object
 	ntp=Ntp();
 	#create a time-stamp object
+	global time;
 	time_stamp=AgTime();
 	#launch a server thread
-	thread.start_new_thread(server,(int(sys.argv[1]),));
+	server_thread=server(int(sys.argv[1]));
+	server_thread.start();
 	while 1:
 		data=raw_input("Enter some text to send");
 		portno=raw_input("Enter portno");
-		send(data,int(portno));	
-		
+		if(data!="quit"):
+			thread.start_new_thread(send,(data,int(portno)));	
+		else:
+			break;
         print "Exiting";
